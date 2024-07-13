089d90e506bebbedc2058620491a6349406ff935
TheQueefOfEngland <KuykendallChase01@gmail.com>
2024-07-13 13:03:10 -0400

    V0.0.5 player movement
    added PC movement, lighting details, VFX and Item/enemy lists

--- K9/Lvls/Dev_Room/k_9_dev_room_controller.gd
+++ K9/Lvls/Dev_Room/k_9_dev_room_controller.gd
@@ -11,11 +11,31 @@ class_name K9_DevRoom
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	set_label_text()
+	
+	
+	
+	#print(str(get_viewport_rect().size))
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
+	
 	sunshine.rotation_degrees += 0.01
 	pass
+
+
+func set_label_text():
+	var text_label_a: RichTextLabel = $RichTextLabel
+	
+	var text_file = FileAccess.open("res://K9/Globals/TxtFiles/TextTest.txt", FileAccess.READ)
+	var content = text_file.get_as_text()
+	
+	text_label_a.text = content
+	
+	pass
+	
+
