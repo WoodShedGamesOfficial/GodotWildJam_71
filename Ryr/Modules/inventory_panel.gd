f289320adbc0b8530c2b2875131b30392db136c8
Gerry Liman <liman.gerry@gmail.com>
2024-07-15 23:17:07 +0700

    Added several detail

--- Ryr/Modules/inventory_panel.gd
+++ Ryr/Modules/inventory_panel.gd
@@ -4,10 +4,21 @@ class_name InventoryPanelUI
 
 @export var item_ui_list : Array[ItemUI]
 
+var selected_index : int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SignalManager.on_item_acquired.connect(on_item_acquired)
 	SignalManager.on_item_lost.connect(on_item_lost)
+	
+	# initial selected
+	item_ui_list[selected_index].set_selected(true)
+
+func _process(delta):
+	if Input.is_action_just_pressed("CycleRight") :
+		cycle_through_item(1)
+	elif Input.is_action_just_pressed("CycleLeft") :
+		cycle_through_item(-1)
 
 func on_item_acquired(item_id : ItemId.Id):
 	for item_ui in item_ui_list :
@@ -25,3 +36,15 @@ func on_item_lost(item_id : ItemId.Id) :
 func reset_ui():
 	for item_ui in item_ui_list :
 		item_ui.reset_ui()
+
+func cycle_through_item(index_offset : int) :
+	item_ui_list[selected_index].set_selected(false)
+	
+	selected_index += index_offset
+	
+	if selected_index >= item_ui_list.size() :
+		selected_index = item_ui_list.size() - 1
+	elif selected_index < 0 :
+		selected_index = 0
+	
+	item_ui_list[selected_index].set_selected(true)
--- Ryr/Modules/item_ui.gd
+++ Ryr/Modules/item_ui.gd
@@ -2,17 +2,27 @@ extends TextureRect
 
 class_name ItemUI
 
+@onready var model = $MarginContainer/Control/Model
+@onready var animation_player = $AnimationPlayer
+
 var is_empty : bool = true
 var item_id : ItemId.Id = ItemId.Id.NONE
 
+var is_selected : bool = false
+
 func initialize(item_texture : Texture2D, item_id : ItemId.Id) :
-	texture = item_texture
+	model.texture = item_texture
 	self.item_id = item_id
 	
 	is_empty = false
 
 func reset_ui():
-	texture = null
+	model.texture = null
 	item_id = ItemId.Id.NONE
 	
 	is_empty = true
+
+func set_selected(is_selected : bool) :
+	self.is_selected = is_selected
+	
+	animation_player.play("None" if !is_selected else "Selected")
